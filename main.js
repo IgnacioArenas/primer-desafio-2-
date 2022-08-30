f7f039fc01f00eb9ec4f4463f5763c824d62e92d
Ignacio Arenas <ignacioarenas2010@hotmail.com>
2022-08-30 19:37:23 -0300

    Primer desafío

---
+++ main.js
@@ -0,0 +1,6 @@
+let comida = prompt("Ingresá tu comida favorita: (si no tenés una, escribí 'salir')");
+
+while(comida != "salir") {
+    console.log(comida);
+    comida = prompt("Ingresá tu comida favorita: (si no tenés una, escribí 'salir')");
+}
